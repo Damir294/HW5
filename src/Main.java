3a50ce442eb86baa02ed619e00d2498a736dc70b
Дамир <damir.obuhov@mail.ru>
2022-11-01 23:22:21 +0400

    ЗАдание 5 вариант 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        var firstBoxer=78.2;
-        var secondBoxer=82.7;
-        var totalWeight=firstBoxer+secondBoxer;
-        var differenceWeight=secondBoxer-firstBoxer;
-
-
-
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        var totalWeight = firstBoxer + secondBoxer;
+        System.out.println("Общий вес бойцов " + totalWeight);
+        var differenceWeight = firstBoxer - secondBoxer;
         System.out.println("Разница между весами бойцов " + differenceWeight);
+        var overload = totalWeight % firstBoxer;
+        System.out.println("Разница между весами бойцов " + overload);
     }
 }
