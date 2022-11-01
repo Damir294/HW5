459601baba231ccbff9d2286ea3dbb6c1842214b
Дамир <damir.obuhov@mail.ru>
2022-11-01 23:06:29 +0400

    ЗАдание 5

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        var firstBoxer=78.2;
+        var secondBoxer=82.7;
+        var totalWeight=firstBoxer+secondBoxer;
+        var differenceWeight=firstBoxer-secondBoxer;
+
+
+
+        System.out.println("Разница между весами бойцов " + differenceWeight);
+    }
+}
